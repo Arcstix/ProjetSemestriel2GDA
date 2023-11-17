042c0e8014090094514e04896b3d68751dca1a1a
Arcstix <morvan.louis2001@gmail.com>
2023-11-17 15:26:08 +0100

    Destruction des enfants au cours du temps
    ajout d'un script attaché aux enfants du player qui leur donne une durée de vie avant de disparaître. Ajout d'une méthode dans SpawnChild qui gère la disparition des entités pour la liste des spawn libre.

---
+++ Assets/Scripts/Scripts_Louis/ChildLifeTime.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildLifeTime : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime = 10f;
+
+    private SpawnChild _refSpawner; // Va permettre de prévenir le spawner quand la durée de vie prend fin.
+    private Transform _refLocation; // Va permettre d'avoir une ref par rapport à l'emplacement utilisé.
+
+    /// <summary>
+    /// Démarre la durée de vie de l'entité. Il possède en paramètre le script qui l'a instantité et la position où il est instantité.
+    /// </summary>
+    /// <param name="spawner"></param>
+    /// <param name="refLocation"></param>
+    public void BeginLifeTime(SpawnChild spawner, Transform refLocation)
+    {
+        _refSpawner = spawner;
+        _refLocation = refLocation;
+        Invoke("EndOfLifeTime", _lifeTime);
+    }
+
+    /// <summary>
+    /// Permet de retirer de la liste l'entité car disparais.
+    /// </summary>
+    private void EndOfLifeTime()
+    {
+        _refSpawner.ClearLocation(_refLocation);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Scripts_Louis/SpawnChild.cs
+++ Assets/Scripts/Scripts_Louis/SpawnChild.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,16 @@ public class SpawnChild : MonoBehaviour
 {
     // Une liste de Transform pour le spawn d'entité.
     [SerializeField] private List<Transform> _spawnList = new List<Transform>();
-    [SerializeField] private GameObject _childEntity;
+    [SerializeField] private ChildLifeTime _childPrefab;
 
     // Cette liste va permettre de savoir les spawner qui sont utilisés.
     private List<Transform> _spawnUsed = new List<Transform>();
 
-    
+
     /// <summary>
     /// Spawn une entité dans l'un des spawns libres du Player.
     /// </summary>
-    public void SpawnEntity()
+    public void SpawnChildEntity()
     {
         foreach (var spawn in _spawnList)
         {
@@ -24,10 +25,20 @@ public class SpawnChild : MonoBehaviour
             if (!_spawnUsed.Contains(spawn))
             {
                 _spawnUsed.Add(spawn); // On ajoute le spawn qui va être utilisé à la liste des spawn utilisé.
-                Instantiate(_childEntity, spawn);
+                ChildLifeTime newChild = Instantiate(_childPrefab, spawn);
+                newChild.BeginLifeTime(this, spawn); // Une fois la création de l'entité on commence la durée de vie de l'entité.
                 return; // On quitte la méthode plus tôt car la méthode a réalisé ce qu'on veut.
             }
         }
+    }
+
+    /// <summary>
+    /// Permet de retirer de la liste des spawns utilisés les entités qui disparaissent.
+    /// </summary>
+    /// <param name="_refLocation"></param>
+    public void ClearLocation(Transform _refLocation)
+    {
+        _spawnUsed.Remove(_refLocation);
     }
 
 }
