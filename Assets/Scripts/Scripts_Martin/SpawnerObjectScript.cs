681cf7dbdd85541b8365717be5b8901a8fb80586
Arcstix <morvan.louis2001@gmail.com>
2023-11-22 14:24:32 +0100

    Changement des noms et correction spawn
    J'ai renommé les mobs maintenant c'est des collectibles et j'ai réduit leur taille. J'ai mis un commentaire sur le script de spawn pour le corriger.

--- Assets/Scripts/Scripts_Martin/SpawnerObjectScript.cs
+++ Assets/Scripts/Scripts_Martin/SpawnerObjectScript.cs
@@ -8,9 +8,8 @@ using static UnityEditor.PlayerSettings;
 //ce Script permet de spawner plusieurs types d'objets dans un espace aléatoirement
 public class SpawnerObjectScript : MonoBehaviour
 {
-    //
-    public float _Timer = 0f;
-    public float _spawnrate = 2f;
+    public float _timer = 0f;
+    public float _spawnRate = 2f;
 
     Vector3 m_min, m_max;
     Collider m_collider;
@@ -27,16 +26,16 @@ public class SpawnerObjectScript : MonoBehaviour
     }
 
     // Un Timer qui spawn un objet  dès qu'il atteint la limite
-    void FixedUpdate()
+    void Update()
     {
-        if (_Timer < _spawnrate)
+        if (_timer < _spawnRate)
         {
-            _Timer += Time.deltaTime;
+            _timer += Time.deltaTime;
         }
         else
         {
             Spawn();
-            _Timer = 0;
+            _timer = 0;
         }
     }
 
@@ -46,6 +45,8 @@ public class SpawnerObjectScript : MonoBehaviour
         int randomIndex = UnityEngine.Random.Range(0, _Entity.Length);
 
         Vector3 SpawnPos = new Vector3(UnityEngine.Random.Range(m_min.x, m_max.x),0, UnityEngine.Random.Range(m_min.z, m_max.z));
+
+        // Tu vas utiliser Navmesh.SamplePosition() dans un if et récupérer la position qu'il retourne. C'est cette position que tu vas te servir pour instantier.
         Instantiate(_Entity[randomIndex], SpawnPos, Quaternion.identity);
     }  
 }
